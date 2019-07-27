0b00d9fe35c1dc6a5310466e6f89e4cf7db62ad8
kerryoo <kerr.yen.yoo@vanderbilt.edu>
2019-07-27 15:40:46 -0400

    imported free models and adjusted scripts to suit game. Thanks to N.Kobayashi

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public float Vertical;
+    public float Horizontal;
+    public bool Jumped;
+    public Vector2 MouseInput;
+
+    public bool Fire1;
+    public bool AimingOn;
+    public bool TestButton;
+
+    public bool ActivatableItem;
+
+    private void Update()
+    {
+
+        MouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        TestButton = Input.GetKeyDown(KeyCode.P);
+        
+        Fire1 = Input.GetButton("Fire1");
+        AimingOn = Input.GetButton("Fire2");
+        Vertical = Input.GetAxis("Vertical");
+        Horizontal = Input.GetAxis("Horizontal");
+        Jumped = Input.GetKeyDown(KeyCode.Space);
+        ActivatableItem = Input.GetKeyDown(KeyCode.R);
+
+
+
+    }
+
+
+}
